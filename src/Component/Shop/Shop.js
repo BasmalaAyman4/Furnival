e90bf02d94c82bee241883f65e962731c5730d08
BasmalaAyman4 <124400018+BasmalaAyman4@users.noreply.github.com>
2023-07-26 02:58:56 +0300

    responsive shop

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -10,10 +10,16 @@ import ShopCard from '../Global/ShopCard/ShopCard';
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 import Pagination from '../Global/Pagination';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 export default function Shop() {
     useEffect(() => {
         Aos.init({ duration: 1000 });
     }, [])
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const [active, setActive] = useState("all")
     const [shopCard, setShopCard] = useState(shopCards)
     const [price, setPrice] = useState(5000);
@@ -64,8 +70,70 @@ export default function Shop() {
                         <h2 className={`${styles.shope__title}`}>Make your dream home true</h2>
                         <p className={`${styles.shope__para}`}>Lorem ipsum dolor sit amet consectetur. Sed tincidunt aliquam feugiat nunc tortor. Elementum sit urna arcu velit. Blandit odio vestibulum id lacus. Pellentesque turpis vel sed mattis a et eget consectetur sit. Sed tincidunt aliquam feugiat nunc tortor. Elementum sit urna arcu velit. Blandit odio vestibulum id lacus. Pellentesque turpis vel sed mattis a et eget consectetur sit</p>
                     </div>
+                    <Button variant="primary" onClick={handleShow} className={`${styles.btnModel}`}>
+                        Show Filter
+                    </Button>
+
+                    <Modal show={show} onHide={handleClose} scrollable={true} className={`${styles.shopFilterModel}`} >
+                        <Modal.Header closeButton >
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Col sm={12} data-aos="fade-left">
+                                <h2 className={`${styles.filter__title}`}>filter by :</h2>
+                                <p className={`${styles.filter__para}`}>price</p>
+                                <div>
+                                    <label>Price (between 5000 and 25000)</label>
+                                    <input type="range" id="vol" name="vol" min="5000" max="25000" step="5000" onInput={handleInput} value={price}></input>
+                                    <div className={`${styles.price__filter}`}>
+                                        <p>5000</p>
+                                        <p>10000</p>
+                                        <p>15000</p>
+                                        <p>20000</p>
+                                        <p>25000</p>
+                                    </div>
+                                </div>
+                                <p className={`${styles.filter__para}`}>rate</p>
+                                <div>
+                                    <label>Rate (between 1 and 5)</label>
+                                    <input type="range" id="vol" name="vol" min="1" max="5" step="1" value="2"></input>
+                                    <div className={`${styles.price__filter}`}>
+                                        <p>1</p>
+                                        <p>2</p>
+                                        <p>3</p>
+                                        <p>4</p>
+                                        <p>5</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className={`${styles.filter__para}`}>category</p>
+                                    <div className={`${styles.category__filter}`}>
+                                        {menuItems.map((val, id) => {
+                                            return (
+                                                <p className={`${active === val ? styles.kind : styles.none}`} key={id} onClick={() => { filterItem(val) }}>{val}</p>
+
+                                            )
+                                        })}
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className={`${styles.filter__para}`}>color</p>
+                                    <div className={`${styles.category__filter}`}>
+                                        {colorItems.map((val, id) => {
+                                            return (
+                                                <p key={id} onClick={() => { filterColor(val) }}> <span className={`${styles.span} ${val}`}>p </span> {val}</p>
+                                            )
+                                        })}
+                                    </div>
+                                </div>
+                            </Col>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                        </Modal.Body>
+
+                    </Modal>
                     <Row>
-                        <Col lg={3} className={`${styles.shopFilter}`} data-aos="fade-left">
+                        <Col md={3} className={`${styles.shopFilter}`} data-aos="fade-up">
                             <h2 className={`${styles.filter__title}`}>filter by :</h2>
                             <p className={`${styles.filter__para}`}>price</p>
                             <div>
